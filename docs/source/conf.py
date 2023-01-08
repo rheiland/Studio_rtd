16c458f7e8ac73d333da821f898d1898696978d9
Randy Heiland <randy.heiland@gmail.com>
2023-01-07 21:48:08 -0500

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PhysiCell Studio'
+copyright = '2023, RandyHeiland, Paul Macklin'
+author = 'Heiland'
 
 release = '0.1'
 version = '0.1.0'
