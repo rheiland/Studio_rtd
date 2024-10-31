5995e98e55aa34271eb30b930e0220ee65209ead
Aydanjb <AydanBondurant@gmail.com>
2024-10-31 18:38:03 -0400

    updated build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -1,31 +1,31 @@
-# Install dependencies
+#!/bin/bash
+
+# Script to build Read the Docs documentation
+
+# Install Dependencies
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
 
-pwd
-ls -lah
-export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
-
+# Build the documentation
 make -C docs clean
 make -C docs html
 
+# Update GitHub pages
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-docroot=`mktemp -d`
-rsync -av "docs/_build/html/" "${docroot}/"
-
-pushd "${docroot}"
+docroot=$(mktemp -d)
+rsync -av "docs/build/html" "${docroot}/"
+pushd "${docroot}" || exit
 
 git init
 git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-gh checkout -b gh-pages
+git checkout -b gh-pages
 
 touch .nojekyll
 
 git add .
-git commit -am "Update docs"
-
+git commit -am "Updating docs for commit ${GITHUB_SHA}"
 git push deploy gh-pages --force
 
-popd
+popd || exit
