85cc7fcff1bc74703a0cb6d655860ff91112c7fd
Aydanjb <AydanBondurant@gmail.com>
2024-10-31 20:13:03 -0400

    updated build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -7,22 +7,26 @@ apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-pip
 
 python3 -m pip install --upgrade
-pip uninstall docutils
-pip install docutils
-
-pwd
-ls docs -lah
 
 # Build the documentation
+## Clean up old assets
 make -C docs clean
-sphinx-build docs/source docs/build/html
 
-# Update GitHub pages
+## Build HTML
+sphinx-build docs/source docs/build/html/
+
+# Update GitHub Pages
+
+## Sets the user to whoever pushed a commit
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
+## Make a temp directory for GitHub Pages
 docroot=$(mktemp -d)
+
+## Copy HTML assets into docroot
 rsync -av "docs/build/html" "${docroot}/"
+
 pushd "${docroot}" || exit
 
 git init
